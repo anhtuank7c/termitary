4efb548018ae14d962cd1f005c87581d63b97d43
Tuan Nguyen <anhtuank7c@hotmail.com>
2025-11-04 16:17:07 +0700

    Add MIT license

---
+++ src/modules/auth/migrate/session.sql
@@ -0,0 +1,5 @@
+CREATE TABLE session (
+	id TEXT NOT NULL PRIMARY KEY,
+	secret_hash BLOB NOT NULL, -- blob is a SQLite data type for raw binary
+	created_at INTEGER NOT NULL -- unix time (seconds)
+) STRICT;
